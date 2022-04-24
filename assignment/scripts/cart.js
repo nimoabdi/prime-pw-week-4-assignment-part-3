f0751ec31bd1368c8b31e98a24825048f52b9f47
Nimo Abdi <nimoabdi318@gmail.com>
2022-04-24 18:00:33 -0500

    created item function basket

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,15 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// created basket and added item function
+let basket = [];
+
+function addItem(item) {
+    basket.push( item ) // this adds a new item to the array
+     console.log('items in my basket');
+     return true;
+  }
+
+ // console.log tests
+ console.log('basket is empty')
